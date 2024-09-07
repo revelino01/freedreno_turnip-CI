83cf1828a89e882d84cd9b9f20c0f1e191568a31
Weab chan <93067085+Weab-chan@users.noreply.github.com>
2024-09-07 04:48:28 -0400

    can't reverse this patch

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -13,10 +13,10 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 #array of string => commit/branch;patch args
 base_patches=(
 	"Quest3;../../patches/quest3.patch;"
-	'disable-VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;--reverse'
+	'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
-	"force_gmem_no_autotuner;../../patches/force_gmem_no_autotuner.patch;"
+	"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 failed_patches=()
 commit=""
