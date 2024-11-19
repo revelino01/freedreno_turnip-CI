f59a294e708653d83e1f1021394d6fa50f79bb43
revelino01 <revelino.adli@gmail.com>
2024-11-19 09:41:08 +0700

    xforyoux patches with clang flag and sdk 35

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -7,7 +7,7 @@ deps="meson ninja patchelf unzip curl pip flex bison zip git"
 workdir="$(pwd)/turnip_workdir"
 packagedir="$workdir/turnip_module"
 ndkver="android-ndk-r27c"
-sdkver="34"
+sdkver="35"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 tagver=""
 
@@ -20,21 +20,21 @@ base_patches=(
 experimental_patches=(
 	"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 	#xforyoux
-    # "visual-fix-issues-in-some-games-1;merge_requests/27986;--reverse"
-	# "visual-fix-issues-in-some-games-2;commit/9de628b65ca36b920dc6181251b33c436cad1b68;--reverse"
-    # "visual-fix-issues-in-some-game-3;merge_requests/28148;--reverse"
-	# "8gen3-fix;merge_requests/27912;--reverse"
-	# "mem-leaks-tu-shader;merge_requests/27847;--reverse"
-    # "add-RMV-Support;commit/a13860e5dfd0cf28ff5292b410d5be44791ca7cc;--reverse"
-	# "fix-color-buffer;commit/782fb8966bd59a40b905b17804c493a76fdea7a0;--reverse"
-    # "Fix-undefined-value-gl_ClipDistance;merge_requests/28109;--reverse"
-	# "tweak-attachment-validation;merge_requests/28135;--reverse"
-	# "Fix-undefined-value-gl_ClipDistance;merge_requests/28109;"
-	# "Add-PC_TESS_PARAM_SIZE-PC_TESS_FACTOR_SIZE;merge_requests/28210;"
-	# "Dont-fast-clear-z-isNotEq-s;merge_requests/28249;"
- 	# "disable-gmem;commit/1ba6ccc51a4483a6d622c91fc43685150922dcdf;--reverse"
-    # "KHR_8bit_storage-support-fix-games-a7xx-break-some-a6xx;merge_requests/28254;"
- 	# "disable-gmem;commit/1ba6ccc51a4483a6d622c91fc43685150922dcdf;--reverse"
+    "visual-fix-issues-in-some-games-1;merge_requests/27986;--reverse"
+	"visual-fix-issues-in-some-games-2;commit/9de628b65ca36b920dc6181251b33c436cad1b68;--reverse"
+    "visual-fix-issues-in-some-game-3;merge_requests/28148;--reverse"
+	"8gen3-fix;merge_requests/27912;--reverse"
+	"mem-leaks-tu-shader;merge_requests/27847;--reverse"
+    "add-RMV-Support;commit/a13860e5dfd0cf28ff5292b410d5be44791ca7cc;--reverse"
+	"fix-color-buffer;commit/782fb8966bd59a40b905b17804c493a76fdea7a0;--reverse"
+    "Fix-undefined-value-gl_ClipDistance;merge_requests/28109;--reverse"
+	"tweak-attachment-validation;merge_requests/28135;--reverse"
+	"Fix-undefined-value-gl_ClipDistance;merge_requests/28109;"
+	"Add-PC_TESS_PARAM_SIZE-PC_TESS_FACTOR_SIZE;merge_requests/28210;"
+	"Dont-fast-clear-z-isNotEq-s;merge_requests/28249;"
+ 	"disable-gmem;commit/1ba6ccc51a4483a6d622c91fc43685150922dcdf;--reverse"
+    "KHR_8bit_storage-support-fix-games-a7xx-break-some-a6xx;merge_requests/28254;"
+ 	"disable-gmem;commit/1ba6ccc51a4483a6d622c91fc43685150922dcdf;--reverse"
 	#weabchan
 	# "Fix-a740;merge_requests/28610;--reverse"
 	# "Handle-non-overlapping-WaW-hazard-with-buffer;merge_requests/28469;"
@@ -201,8 +201,8 @@ build_lib_for_android(){
 	cat <<EOF >"android-aarch64"
 [binaries]
 ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang', '-O2', '-flto']
-cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '-O2', '-flto']
+c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang', '-fno-semantic-interposition', '-O2', '-flto']
+cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-semantic-interposition', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '-O2', '-flto']
 c_ld = 'lld'
 cpp_ld = 'lld'
 strip = '$ndk/aarch64-linux-android-strip'
