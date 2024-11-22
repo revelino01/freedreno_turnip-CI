23868682fcf940ebc72ceb7a43fc1355881eec0a
revelino01 <revelino.adli@gmail.com>
2024-11-22 21:43:10 +0700

    build for mesa 24.3.0 stable

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -9,7 +9,7 @@ packagedir="$workdir/turnip_module"
 ndkver="android-ndk-r27c"
 sdkver="33"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
-tagver="mesa-24.2.7"
+tagver="mesa-24.3.0"
 
 #array of string => commit/branch;patch args
 #these are some changes that are not merge, the --reverse is reversing merged changes
