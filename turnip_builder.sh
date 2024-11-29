7b3d53f96dd322917b3ce955dc6e13b177075660
Weab chan <93067085+Weab-chan@users.noreply.github.com>
2024-11-29 16:16:31 -0500

    removed merged code

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -12,7 +12,6 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 
 #array of string => commit/branch;patch args
 base_patches=(
-	"Quest3;../../patches/quest3.patch;"
 	'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
